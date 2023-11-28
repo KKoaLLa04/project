1f31499ea94c17c3f9fd127e4ab8415bbe1d4403
DJBigH <149777866+DJBigH@users.noreply.github.com>
2023-11-28 16:01:11 +0700

    Update home.php\

--- clients/home/model/home.php
+++ clients/home/model/home.php
@@ -14,6 +14,7 @@ function getAllCate()
     return $data;
 }
 
+
 function getAllCourseId($id)
 {
     $sql = "SELECT course.*, fullname, course_category.title as cate_name FROM course INNER JOIN teacher ON teacher.id=course.teacher_id INNER JOIN course_category ON course_category.id=course.cate_id WHERE course.cate_id = '$id' ORDER BY course.cate_id DESC";
